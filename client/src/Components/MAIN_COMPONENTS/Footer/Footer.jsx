0ea9673807d1c27564d13949df09832660aea07c
user name <you@example.com>
2023-06-13 02:01:51 +0400

    navbar and  part of footer created

--- client/src/Components/MAIN_COMPONENTS/Footer/Footer.jsx
+++ client/src/Components/MAIN_COMPONENTS/Footer/Footer.jsx
@@ -1,8 +1,74 @@
 import React from 'react'
+import FooterStyle from './Footer.module.css'
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import TelegramIcon from '@mui/icons-material/Telegram';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GoogleIcon from '@mui/icons-material/Google';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className={FooterStyle.footer}>
+      <Container maxWidth='lg'>
+        <section className={FooterStyle.first_section}>
+
+        
+        <Grid container spacing={2}>
+  <Grid xs={3}>
+      <Link to= '/'><img src='http://sbtechnosoft.com/guidepro/images/footer-logo.png'></img></Link> 
+  </Grid>
+  <Grid xs={9}>
+   <div style={{display : 'flex', alignItems : 'center', justifyContent : 'end'}}>
+    <input className={FooterStyle.input} type='email'  placeholder='Enter Email Adress' /> 
+    <button type='submit' className={FooterStyle.button}>SUBCRIBE</button>
+   </div>
+  </Grid>
+ 
+</Grid>
+        
+        
+        </section>
+        <section className={FooterStyle.second_section}>
+        <Grid container spacing={2}>
+          <Grid xs={4}>
+            <div className={FooterStyle.second_section_div}>
+              <h3 className={FooterStyle.h3} >Need Help?</h3>
+              <div className={FooterStyle.title1} >
+<LocalPhoneIcon/> <span>CALL US : </span> <span> <b>+994-70-431-17-12</b> </span>
+              </div>
+            </div>
+          </Grid>
+          <Grid xs={4}>
+            <div className={FooterStyle.second_section_div}>
+              <h3 className={FooterStyle.h3} >Email : </h3>
+              <div className={FooterStyle.title1} >
+              <TelegramIcon /> <span style={{margin : '0px, 10px'}} > info@test.com </span> <span> <b>/</b> </span> <span> <b>78rj6f4@code.edu.az</b> </span>
+              </div>
+            </div>
+          </Grid>
+          <Grid xs={4}>
+            <div className={FooterStyle.second_section_div}>
+              <h3 className={FooterStyle.h3} >Need Help?</h3>
+              <div className={FooterStyle.title1} >
+<FacebookOutlinedIcon/>  <TwitterIcon/> <GoogleIcon/> <LinkedInIcon/> <YouTubeIcon/>
+              </div>
+            </div>
+          </Grid>
+
+          </Grid>
+
+        </section>
+        <section className={FooterStyle.third_section}>
+
+        </section>
+        </Container>
+    </footer>
   )
 }
 
--- client/src/Components/MAIN_COMPONENTS/Navbar/Navbar.jsx
+++ client/src/Components/MAIN_COMPONENTS/Navbar/Navbar.jsx
@@ -1,32 +1,116 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import {Link} from 'react-router-dom';
-import NavbarStyle from './Navbar.module.css'
+import { Link } from 'react-router-dom';
+import NavbarStyle from './Navbar.module.css';
+import Container from '@mui/material/Container';
+import { Dropdown } from 'antd';
+
+
+
+const items = [
+  {
+    key: '1',
+    label: (
+      <Link to='aboutus'>ABOUT US</Link>
+    ),
+  },
+  {
+    key: '2',
+    label: (
+      <Link to='ourteam'>OUR TEAM</Link>
+
+    ),
+  },
+  {
+    key: '3',
+    label: (
+      <Link to='pricing'>PRICING</Link>
+
+    ),
+  },
+];
+// const items = [
+//   {
+//     key: '1',
+//     label: (
+//       <Link to='joblist'>JOB LIST</Link>
+//     ),
+//   },
+//   {
+//     key: '2',
+//     label: (
+//       <Link to='jobdetails'>JOB DETAILS</Link>
+
+//     ),
+//   },
+  
+// ];
+
 
 
 
 
 function Navbar() {
+
+  
   return (
-    <Box sx={{ flexGrow: 1 }} >
-      <Grid container spacing={2}>
-        <Grid item xs={4} md={4}>
-         <Link to = '/home'>  <img src='http://sbtechnosoft.com/guidepro/images/logo.png' alt='#'></img></Link>
+    <Container maxWidth='lg' style={{position : 'sticky'}}>
+      <Box sx={{ flexGrow: 1 }} style={{ backgroundColor: '#ffffff', margin: '.8rem 1rem' }} >
+        <Grid container spacing={2}>
+          <Grid item xs={4} md={4}>
+            <Link to='/home'>  <img src='http://sbtechnosoft.com/guidepro/images/logo.png' alt='#'></img></Link>
+          </Grid>
+          <Grid item xs={8} md={8} style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+            <Grid item xs={8} md={8} >
+              <ul style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
+                <li style={{ listStyle: 'none' }}   > <Link className={NavbarStyle.a} style={{ textDecoration: 'none', transition: 'all 0.3s ease 0s' }} to='/home'>HOME</Link> </li>
+                <li style={{ listStyle: 'none' }}  > <Link className={NavbarStyle.a} style={{ textDecoration: 'none', transition: 'all 0.3s ease 0s' }} to='/aboutus'>ABOUT US</Link> </li>
+                <li style={{ listStyle: 'none' }}  > <Dropdown
+                  menu={{
+                    items,
+                    mode : 'vertical',
+
+                  }}
+                  placement="bottom"
+                >
+                  <Link className={NavbarStyle.a} style={{ textDecoration: 'none', transition: 'all 0.3s ease 0s' }} to='/'>PAGES</Link>
+                </Dropdown> </li>
+                <li style={{ listStyle: 'none' }}  > <Dropdown
+                  menu={{
+                    mode : 'vertical',
+                    items
+                  }}
+                  placement="bottom"
+                >
+                  <Link className={NavbarStyle.a} style={{ textDecoration: 'none', transition: 'all 0.3s ease 0s' }} to='/'>JOBS</Link>
+                </Dropdown> </li>
+                <li style={{ listStyle: 'none' }}  > <Dropdown
+                  menu={{
+                    mode : 'vertical',
+                    items
+                  }}
+                  placement="bottom"
+                >
+                  <Link className={NavbarStyle.a} style={{ textDecoration: 'none', transition: 'all 0.3s ease 0s' }} to='/'>BLOG</Link>
+                </Dropdown> </li>
+                <li style={{ listStyle: 'none' }}  > <Link className={NavbarStyle.a} style={{ textDecoration: 'none', transition: 'all 0.3s ease 0s' }} to='/contactus'>CONTACT US</Link> </li>
+                
+              </ul>
+            </Grid>
+            <Grid item xs={4} md={4}>
+              <div className={NavbarStyle.opsi}>
+                <div className={NavbarStyle.div_link}>
+                  <Link className={NavbarStyle.link} to='login'>LOGIN/REGISTER</Link>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+
+
         </Grid>
-        <Grid item xs={5} md={5} style={{display : 'flex', justifyContent : 'space-around', alignItems : 'center'}}>
-         <ul style={{display : 'flex', justifyContent : 'space-around', alignItems : 'center'}}>
-          <li style={{listStyle : 'none'}}   > <Link className={NavbarStyle.a}   style={{textDecoration : 'none', transition: 'all 0.3s ease 0s'}} to= '/home'>HOME</Link> </li>
-          <li style={{listStyle : 'none'}}  > <Link  className={NavbarStyle.a}  style={{textDecoration : 'none', transition: 'all 0.3s ease 0s'}} to= '/aboutus'>ABOUT US</Link> </li>
-          <li style={{listStyle : 'none'}}  > <Link  className={NavbarStyle.a}  style={{textDecoration : 'none', transition: 'all 0.3s ease 0s'}} to= '/'>PAGES</Link> </li>
-          <li style={{listStyle : 'none'}}  > <Link  className={NavbarStyle.a}  style={{textDecoration : 'none', transition: 'all 0.3s ease 0s'}} to= '/'>JOBS</Link> </li>
-          <li style={{listStyle : 'none'}}  > <Link  className={NavbarStyle.a}  style={{textDecoration : 'none', transition: 'all 0.3s ease 0s'}} to= '/'>BLOG</Link> </li>
-          <li style={{listStyle : 'none'}}  > <Link  className={NavbarStyle.a}  style={{textDecoration : 'none', transition: 'all 0.3s ease 0s'}} to= '/contactus'>CONTACT US</Link> </li>
-         </ul>
-        </Grid>
-       
-      </Grid>
-    </Box>
+      </Box>
+    </Container>
   )
 }
 
